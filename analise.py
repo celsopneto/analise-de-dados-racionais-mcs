3c83783664a243263278f16b3d945ef6d82503f6
Celso Neto <celsopneto@hotmail.com>
2019-10-22 18:08:39 +0800

    volta amanha que tem mais

--- analise.py
+++ analise.py
@@ -15,35 +15,48 @@ pd.options.display.max_rows = 100
 pd.options.display.max_columns = 50
 caminho_arquivo = "scrapping" + os.sep + "data.json"
 df = pd.read_json(caminho_arquivo)
-df_analise = df.loc[~df['letra'].isnull()].copy()
 
-def analise_lemma(doc):
-    lemmas = [token.lemma_ for token in doc]
-    poss = [token.pos_ for token in doc]
-    deps = [token.dep_ for token in doc]
-    df = pd.DataFrame({"lemma":lemmas,
-                       "tag": poss,
-                       "dep": deps
-                       }
-    )
+df_letras = df.loc[~df['letra'].isnull()].copy()
+
+
+class NlpLetra:
+    """
+        Objeto que busca no modulo spaCy a interpretaçao da letra.
+    """
+
+    def __init__(self, letra):
+        self.__nlp = nlp(letra)
+        self.__excluir = ["SPACE", "PUNCT"]
+
+    def texts(self):
+        return [token.text for token in self.__nlp \
+                if token.pos_ not in self.__excluir]
+
+    def pospeech(self):
+        return [token.pos_ for token in self.__nlp\
+                if token.pos_ not in self.__excluir]
+
+    def deps(self):
+        return [token.dep_ for token in self.__nlp\
+                if token.pos_ not in self.__excluir]
+
+    def ents(self):
+        return [(entity, entity.label_) for entity in self.__nlp.ents]
+
+
+df_letras["nlp_obj"] = df_letras.apply(lambda x: NlpLetra(x['letra']),
+                                        axis=1)
+df_letras["total_palavras"] = df_letras.apply(lambda x:
+    len(x['nlp_obj'].texts()), axis=1)
+df_letras["palavras_unicas"] = df_letras.apply(lambda x:
+    len(set(x['nlp_obj'].texts())), axis=1)
+
+#t = df.iloc[0]['letra']
+#test = nlp(t)
+#texts = [token.text for token in test]
+#pos = [token.pos_ for token in test]
+#deps = [token.dep_ for token in test]
+## CAMINHO IMPORTANTE, Reconhecimento de entidades mencionadas (REM)
+## https://teses.usp.br/teses/disponiveis/45/45134/tde-23052013-104248/publico/dissertacao_rem_wesley_seidel.pdf
+#ents = [(entity, entity.label_) for entity in test.ents]
     
-    return df.groupby("lemma").agg({"tag": lambda x: list(x),
-                                    "dep": lambda x: list(x)
-                                    }).reset_index()
-    
-for i in range(len(df_analise)):
-    print("""
-          {}.
-          
-          {}
-          ---------""".format(df_analise.iloc[i]['Canção'], 
-          df_analise.iloc[i]['letra']))
-    
-t = df_analise.iloc[0]['letra'].strip()
-test = nlp(t)
-texts = [token.text for token in test]
-pos = [token.pos_ for token in test]
-deps = [token.dep_ for token in test]
-# CAMINHO IMPORTANTE, Reconhecimento de entidades mencionadas (REM)
-# https://teses.usp.br/teses/disponiveis/45/45134/tde-23052013-104248/publico/dissertacao_rem_wesley_seidel.pdf
-ents = [(entity, entity.label_) for entity in test.ents]
