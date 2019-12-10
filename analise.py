b97d35b95bf951affaef2ec43547b2f2b5c90c0d
Celso Neto <celsopneto@hotmail.com>
2019-12-10 07:45:00 -0300

    análise em progresso

--- analise.py
+++ analise.py
@@ -16,9 +16,11 @@ pd.options.display.max_columns = 50
 caminho_arquivo = "scrapping" + os.sep + "data.json"
 df = pd.read_json(caminho_arquivo, encoding='utf-8')
 df = df[['Canção', 'Participantes', 'Álbum', 'letra']]
-df['Participantes'] = df.apply(lambda x:
-    [el.strip() for el in x['Participantes'].split(",")], axis=1)
-df_letras = df.loc[~df['letra'].isnull()].copy()
+# testar o get_dummies()
+#df['Participantes'] = df.apply(lambda x:
+#    [el.strip() for el in x['Participantes'].split(",")], axis=1)
+    
+df_letras = df.loc[~df['letra'].isnull()].copy().reset_index(drop=True)
 
 
 class NlpLetra:
@@ -29,6 +31,8 @@ class NlpLetra:
     def __init__(self, letra):
         self.__nlp = nlp(letra)
         self.__excluir = ["SPACE", "PUNCT"]
+        self.__pos_dict()
+        
 
     def texts(self):
         return [token.text for token in self.__nlp \
@@ -42,6 +46,14 @@ class NlpLetra:
     def tags(self):
         return [token.tag_ for token in self.__nlp\
                 if token.pos_ not in self.__excluir]
+        
+    def __pos_dict(self):
+        self.pos_dict =   {
+         "pos": self.pospeech(),
+         "tag": self.tags(),
+         "text": self.texts()
+         }
+        
     def deps(self):
         return [token.dep_ for token in self.__nlp\
                 if token.pos_ not in self.__excluir]
@@ -61,30 +73,35 @@ df_letras["palavras_unicas"] = df_letras.apply(lambda x:
 lista_albums = list(df['Álbum'].unique())
 
 
-t = df_letras.iloc[0]
-# 15 diário de um detento
-nlp_t = df_letras.iloc[15, 4]
+# 15 diário de um detento 4 é a ref da coluna nlp_obj
+
 
 # Este df_nlp parece uma boa forma de analizar
 # teste com 1 por amor 2 por dinheiro já mostra algumas inconsistências no
 # reconhecimento. Ex deus como conjunção subordinativa ("pos SCONJ")
 # irmãos e olhos em pos SYM
 # alguns DET em ADJ em diário de um detento
+# A solução é basicamente treinar outro modelo?
+
+
+#nlp_t = df_letras.iloc[15, 4]
+#df_nlp = pd.DataFrame({"pos":nlp_t.pospeech(),
+#                       "tag":nlp_t.tags(),
+#                       "text": nlp_t.texts()})
+#
+#print(df_nlp['pos'].value_counts())
 
 
 
-df_nlp = pd.DataFrame({"pos":nlp_t.pospeech(),
-                       "tag":nlp_t.tags(),
-                       "text": nlp_t.texts()})
-print(df_nlp['pos'].value_counts())
 
-def df_postag(df, tag):
-    return df_nlp.loc[df_nlp["pos"] == tag]
+
+#def df_postag(df, tag):
+#    return df_nlp.loc[df_nlp["pos"] == tag]
+#
 
 #df_albums = df_letras.groupby('Álbum')
 #
-## CAMINHO IMPORTANTE, Reconhecimento de entidades mencionadas (REM)
-## 
+
 # após correção, apenas os albums importantes ficaram aparecendo
 # verificados, Escolha seu caminho que tem 2 letras
 # no EP repete 3x a musica voz ativa 
