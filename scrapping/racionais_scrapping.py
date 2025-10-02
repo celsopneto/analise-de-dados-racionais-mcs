311efedb226542b03392b9ca012d1c981f5de759
Celso Pereira Neto <celsopneto@hotmail.com>
2025-10-01 22:36:09 -0300

    atualização para novas tags  e melhor preprocessamento

--- scrapping/racionais_scrapping.py
+++ scrapping/racionais_scrapping.py
@@ -11,7 +11,10 @@ import json
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from fuzzywuzzy import fuzz
+from rapidfuzz import fuzz
+from io import StringIO
+
+
 start = dt.datetime.now()
 
 class InfoLetra:
@@ -26,7 +29,7 @@ class InfoLetra:
         """
         self.__bs_letra = BeautifulSoup(requests.get(link_letra).content,
                                         'html.parser')
-        self.__titulo = self.__bs_letra.find("h1", "head-title").string
+        self.__titulo = self.__bs_letra.find("h1", "textStyle-primary").string
         self.__letra_finds = self.__bs_letra\
         .find("div", class_="lyric-original").find_all('p')
 
@@ -34,7 +37,7 @@ class InfoLetra:
         """
             Retorna o título da música.
         """
-        return str(self.__titulo)
+        return str(self.__titulo).replace("\n","").strip()
 
 
     def letra(self):
@@ -58,7 +61,7 @@ class InfoLetra:
         for char in para_trocar:
             letra = letra.replace(char, ",")
 
-        return  letra
+        return  letra.replace("\n","").strip()
 
     def reseta_titulo(self, novo_titulo):
         """
@@ -83,24 +86,38 @@ def recupera_letra(row, a_infos_letras):
     row['letra'] = ''
     row['metodo'] = ''
     row['ratio'] = 0
-    if row['Álbum'] == "Cores & Valores":
-        row['Canção'] = row['Canção'].replace("&", "e").replace(":", "")
+    row['Canção'] = preprocessar_titulo(row['Canção'] )
+   
+    
 
-    for info_letra in a_infos_letras:
+    for info_letra in a_infos_letras[:]:
+        if  row['Canção'] == 'cores e valores finado "neguin"':
+            print("found")
         if info_letra.titulo() == row['Canção']:
             row['titulo_encontrado'] = info_letra.titulo()
             row['letra'] = info_letra.letra()
             row['metodo'] = 'igualdade'
             row['ratio'] = fuzz.ratio(info_letra.titulo(), row['Canção'])
+            a_infos_letras.remove(info_letra)
+            return row
         elif fuzz.ratio(info_letra.titulo(), row['Canção']) > 80 and \
         row['letra'] == '':
             row['titulo_encontrado'] = info_letra.titulo()
             row['letra'] = info_letra.letra()
             row['metodo'] = 'fuzzy'
             row['ratio'] = fuzz.ratio(info_letra.titulo(), row['Canção'])
+            a_infos_letras.remove(info_letra)
+            return row
+
 
     return row
-
+def preprocessar_titulo(titulo):
+    return (titulo.lower()
+            .replace('(part. dj cia)','')
+            .replace('&', 'e')
+            .replace('pt.', 'pt')
+            .replace(':', '')
+            .strip())
 def arruma_albums(row):
     album = row['Álbum']
     for item in configs["albums_rm"]:
@@ -118,7 +135,8 @@ with open("configs.json", encoding='utf-8') as file:
 
 MENU_LETRAS = BeautifulSoup(requests.get(configs['MENU_LETRAS']).content,
                             'html.parser')
-WIKI = BeautifulSoup(requests.get(configs['WIKIPAGE']).content, 'html.parser')
+wiki_headers ={'User-Agent': 'RacionaisScrapper/0.1 (https://github.com/celsopneto/analise-de-dados-racionais-mcs; celsopneto@hotmail.com)'}
+WIKI = BeautifulSoup(requests.get(configs['WIKIPAGE'], headers=wiki_headers).content,  'html.parser')
 TABELA_WIKI = WIKI.find("table", "wikitable")
 links_letras = MENU_LETRAS.find(id='cnt-artist-songlist').find_all('a')
 
@@ -132,7 +150,7 @@ tabela_html.body.append(TABELA_WIKI)
 
 # necessidade do [0]
 # pd.read_html() retorna uma lista de DataFrames
-songs_wiki_df = pd.read_html(str(tabela_html))[0]
+songs_wiki_df = pd.read_html(StringIO(str(tabela_html)))[0]
 songs_wiki_df = songs_wiki_df.apply(arruma_albums, axis=1)
 links_d = {}
 for i, link  in enumerate(links_letras):
@@ -143,10 +161,14 @@ erradas_conhecidas = configs['dict_erradas']
 infos_letras = []
 
 for (i, item) in enumerate(links_d.items()):
-    infos_letras.append(InfoLetra(item[1]))
-    titulo = infos_letras[i].titulo()
-    if titulo in configs['dict_erradas'].keys():
-        infos_letras[i].reseta_titulo(configs['dict_erradas'][titulo])
+    info_letra = InfoLetra(item[1])
+    info_letra.reseta_titulo(preprocessar_titulo(titulo=info_letra.titulo()))
+
+    if info_letra.titulo() in configs['dict_erradas'].keys():
+        info_letra.reseta_titulo(configs['dict_erradas'][info_letra.titulo()])
+
+    infos_letras.append(info_letra)
+
 
 infos_letras = [song for song in infos_letras if song.titulo() \
               not in falsas_conhecidas]
@@ -158,9 +180,9 @@ songs_wiki_letras = songs_wiki_df.loc[~songs_wiki_df['Canção']\
 songs_wiki_letras = songs_wiki_letras.apply(lambda x: recupera_letra(x, infos_letras),
                                             axis=1)
 
-result = songs_wiki_letras.append(songs_wiki_df.loc[songs_wiki_df['Canção']\
-                                  .isin(configs["instrumentais_misc"])],\
-                                  sort=False)
+
+s2 =songs_wiki_df.loc[songs_wiki_df['Canção'].isin(configs["instrumentais_misc"])]
+result = pd.concat([songs_wiki_letras, s2], ignore_index=True)                                  
 
 with open('data.json', 'w', encoding='utf-8') as file:
     result.to_json(file, force_ascii=False)
