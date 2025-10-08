92dda6076967a28dc34ad515df22d413396d41b1
Celso Pereira Neto <celsopneto@hotmail.com>
2025-10-08 07:39:54 -0300

    fix nome funcao densidade_lexical

--- analise.py
+++ analise.py
@@ -5,6 +5,7 @@ Created on Thu Oct 17 16:24:24 2019
 
 @author: Celso Pereira Neto
 """
+import json
 import os
 import pandas as pd
 import spacy
@@ -35,7 +36,8 @@ total_participantes = [nome for nomes in listed for nome in nomes]
 
 participantes_unique = list(set(total_participantes))
 
-df_letras = df.loc[~df['letra'].isnull()].copy().reset_index(drop=True)
+df_letras = df.loc[(~df['letra'].isnull()) &(~(df['letra'] ==''))].copy().reset_index(drop=True)
+
 
 
 class NlpLetra:
@@ -75,7 +77,8 @@ class NlpLetra:
     def ents(self):
         return [(entity, entity.label_) for entity in self.__nlp.ents]
     
-def densidade_lirica(row):
+def densidade_lexical(row):
+    CATEGORIAS_CONTEUDO = ['NOUN', 'PROPN', 'ADJ', 'VERB', 'ADV']
     """Calcula métricas específicas de densidade lírica"""
     nlp_t = row['nlp_obj']
     df_nlp = pd.DataFrame({"pos":nlp_t.pospeech(),
@@ -83,33 +86,21 @@ def densidade_lirica(row):
                        "text": nlp_t.texts()})
     
     # Densidade nominal (substantivos e nomes próprios)
-    densidade_nominal = len(df_nlp[df_nlp['pos'].isin(['NOUN', 'PROPN'])]) / len(df_nlp)
+    #densidade_nominal = len(df_nlp[df_nlp['pos'].isin(['NOUN', 'PROPN'])]) / len(df_nlp)
     
-    # Densidade adjetival (palavras descritivas)
-    densidade_adjetival = len(df_nlp[df_nlp['pos'] == 'ADJ']) / len(df_nlp)
-    
-    # Densidade verbal (ações e estados)
-    densidade_verbal = len(df_nlp[df_nlp['pos'] == 'VERB']) / len(df_nlp)
-    
-    # Densidade adverbial (modificadores expressivos)
-    densidade_adverbial = len(df_nlp[df_nlp['pos'] == 'ADV']) / len(df_nlp)
-
-    row['densidade_nominal'] = densidade_nominal
-    row['densidade_adjetival'] = densidade_adjetival
-    row['densidade_verbal'] =  densidade_verbal
-    row['densidade_adverbial'] = densidade_adverbial,
-    row['densidade_total'] = densidade_nominal + densidade_adjetival + densidade_verbal + densidade_adverbial
+   
+    row['densidade_lexical'] = len(df_nlp[df_nlp['pos'].isin(CATEGORIAS_CONTEUDO)]) / len(df_nlp)
     
     return row
 
 df_letras["nlp_obj"] = df_letras.apply(lambda x: NlpLetra(x['letra']),
                                         axis=1)
-df_letras["total_palavras"] = df_letras.apply(lambda x:
-    len(x['nlp_obj'].texts()), axis=1)
-df_letras["palavras_unicas"] = df_letras.apply(lambda x:
-    len(set(x['nlp_obj'].texts())), axis=1)
+# df_letras["total_palavras"] = df_letras.apply(lambda x:
+#     len(x['nlp_obj'].texts()), axis=1)
+# df_letras["palavras_unicas"] = df_letras.apply(lambda x:
+#     len(set(x['nlp_obj'].texts())), axis=1)
 
-df_letras["diversidade_lexica"] = df_letras.apply(densidade_lirica, axis=1)
+df_letras = df_letras.apply(densidade_lirica, axis=1)
 
 
 lista_albums = list(df['Álbum'].unique())
@@ -121,51 +112,24 @@ nlp_t = df_letras.iloc[15, 4]
 df_nlp = pd.DataFrame({"pos":nlp_t.pospeech(),
                        "tag":nlp_t.tags(),
                        "text": nlp_t.texts()})
-def densidade_lirica(df_nlp):
-    """Calcula métricas específicas de densidade lírica"""
-    
-    
-    # Densidade nominal (substantivos e nomes próprios)
-    densidade_nominal = len(df_nlp[df_nlp['pos'].isin(['NOUN', 'PROPN'])]) / len(df_nlp)
-    
-    # Densidade adjetival (palavras descritivas)
-    densidade_adjetival = len(df_nlp[df_nlp['pos'] == 'ADJ']) / len(df_nlp)
-    
-    # Densidade verbal (ações e estados)
-    densidade_verbal = len(df_nlp[df_nlp['pos'] == 'VERB']) / len(df_nlp)
-    
-    # Densidade adverbial (modificadores expressivos)
-    densidade_adverbial = len(df_nlp[df_nlp['pos'] == 'ADV']) / len(df_nlp)
-    
-    return {
-        'densidade_nominal': densidade_nominal,
-        'densidade_adjetival': densidade_adjetival,
-        'densidade_verbal': densidade_verbal,
-        'densidade_adverbial': densidade_adverbial,
-        'densidade_total': densidade_nominal + densidade_adjetival + densidade_verbal + densidade_adverbial
-    }
-
-
-
-        
-        
-        
-    
-
-print(df_nlp['pos'].value_counts())
-
-
-
-
-
-
-
 
 # após correção, apenas os albums importantes ficaram aparecendo
 # verificados, Escolha seu caminho que tem 2 letras
 # no EP repete 3x a musica voz ativa
 
-#df_albums = df_letras.groupby('Álbum')
+with open("scrapping" + os.sep + "configs.json", encoding='utf-8') as file:
+    configs = json.load(file)
+
+datas_albums = configs['datas_albums']
+df_albums = df_letras.groupby('Álbum', as_index=False).agg({
+    'Canção':'count',
+    'Participantes': 'sum',
+    'densidade_lexical': 'mean' }).rename(columns={
+        'Canção': 'num_letras',
+        'Participantes': 'total_participantes'
+        })
+df_albums['ano'] = df_albums['Álbum'].map(datas_albums)
+df_albums = df_albums.sort_values(by='ano')
 #
 #  Album                               num_letras
 # Nada como um Dia após o Outro Dia    20
